d6ef9c1bff31f6123f4b1147c5808f0bc6e2f2ec
Deepanjali Ishte <159618789+DeepanjaliIshte@users.noreply.github.com>
2024-05-10 23:33:07 +0530

    Add files via upload

---
+++ Input_Value.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Input_Value {
+
+	public static void main (String[] args) {
+		Scanner sn=new Scanner(System.in);
+		System.out.println("Enter  Name");
+		String name= sn.next();
+		System.out.println("Enter Gender");
+		char Gender =sn.next().charAt(0);
+		System.out.println("Enter Age");
+		int age=sn.nextInt();
+		System.out.println("Enter Contact number");
+		long ph_no=sn.nextLong();
+		System.out.println();
+		System.out.println("Name : "+name);
+		System.out.println("Gender : "+Gender);
+		System.out.println("Age: "+age);
+		System.out.println("Contact No.: "+ph_no);
+	}
+
+}
+
+deepanjaliishte@gmail.com
+Deepanjali07..#
+
+deepa.ishte@gmail.com
+Deepanjali0701..#
+
+SD8956SD0000?
